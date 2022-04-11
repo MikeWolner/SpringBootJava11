7a328b20e5ef6b447945a6e3198ec2b9a9e0828b
Mike Wolner <mikewdamaso@gmail.com>
2022-04-11 04:34:39 -0300

    exception handling - delete

---
+++ src/main/java/com/trazaglock/course/services/Exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.trazaglock.course.services.Exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
